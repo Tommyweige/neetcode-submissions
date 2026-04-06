21fa9f4464cdd4e1c62306dc21c59b55bcb226ab
Tommy Pan <62233340+Tommyweige@users.noreply.github.com>
2026-04-06 17:13:02 +0800

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.py
@@ -0,0 +1,33 @@
+class link:
+    def __init__(self,key: int):
+        self.key = key
+        self.next = None
+
+class MyHashSet:
+
+    def __init__(self):
+        self.bucket = [link(0) for i in range(10**4)]
+
+    def add(self, key: int) -> None:
+        cur = self.bucket[key % len(self.bucket)]
+        while cur.next:
+            if cur.next.key == key:
+                return
+            cur = cur.next
+        cur.next = link(key)
+
+    def remove(self, key: int) -> None:
+        cur = self.bucket[key % len(self.bucket)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
+        
+    def contains(self, key: int) -> bool:
+        cur = self.bucket[key % len(self.bucket)]
+        while cur.next:
+            if cur.next.key == key:
+                return True
+            cur = cur.next
+        return False
